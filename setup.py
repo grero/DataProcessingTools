70cbf366a2bea8e6ebabd73ede9de1c569f3a1f5
roger <roger.herikstad@gmail.com>
2020-06-12 07:35:18 +0800

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup = (name="DataProcessingTools",
+         version="0.1.0",
+         description="""Tools for processing data with hierarchical organization""",
+         url="https://github.com/grero/DataProcessingTools.git",
+         author="Roger Herikstad",
+         author_email="roger.herikstad@gmail.com",
+         license="MIT",
+         packages=["DataProcessingTools"],
+         include_package_data=True
+         )
