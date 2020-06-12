af9c79711431ed3718345ca95b318e87464b3c54
roger <roger.herikstad@gmail.com>
2020-06-12 13:21:40 +0800

    Fixes a bug in setup

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
-setup = (name="DataProcessingTools",
-         version="0.1.1",
-         description="""Tools for processing data with hierarchical organization""",
-         url="https://github.com/grero/DataProcessingTools.git",
-         author="Roger Herikstad",
-         author_email="roger.herikstad@gmail.com",
-         license="MIT",
-         packages=["DataProcessingTools"],
-         include_package_data=True
-         )
+setup(name="DataProcessingTools",
+      version="0.1.1",
+      description="""Tools for processing data with hierarchical organization""",
+      url="https://github.com/grero/DataProcessingTools.git",
+      author="Roger Herikstad",
+      author_email="roger.herikstad@gmail.com",
+      license="MIT",
+      packages=["DataProcessingTools"],
+      include_package_data=True
+      )
