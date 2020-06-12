5bb6f53d86ba91a4ec217591026fe9877d79e9b8
roger <roger.herikstad@gmail.com>
2020-06-12 08:19:49 +0800

    Bumps version number

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup = (name="DataProcessingTools",
-         version="0.1.0",
+         version="0.1.1",
          description="""Tools for processing data with hierarchical organization""",
          url="https://github.com/grero/DataProcessingTools.git",
          author="Roger Herikstad",
