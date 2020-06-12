a08f306d8bd6d32f6f1cfbd72bdc3e94ee46597c
roger <roger.herikstad@gmail.com>
2020-06-13 07:45:21 +0800

    Adds test for resolve_level

--- tests/test_levels.py
+++ tests/test_levels.py
@@ -1,6 +1,10 @@
 import DataProcessingTools as DPT
 
+
 def test_level():
     cwd = "Pancake/20130923/session01/array02/channel33"
     ll = DPT.levels.level(cwd)
     assert ll == "channel"
+
+    rr = DPT.levels.resolve_level("session", cwd)
+    assert rr == "./../../.."
