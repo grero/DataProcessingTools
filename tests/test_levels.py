7998e54eb1df2d5d0442a35e693570fe32a58883
roger <roger.herikstad@gmail.com>
2020-06-13 19:34:40 +0800

    adds more tests

--- tests/test_levels.py
+++ tests/test_levels.py
@@ -1,4 +1,6 @@
 import DataProcessingTools as DPT
+import tempfile
+import os
 
 
 def test_level():
@@ -8,3 +10,23 @@ def test_level():
 
     rr = DPT.levels.resolve_level("session", cwd)
     assert rr == "./../../.."
+
+    tdir = tempfile.gettempdir()
+    cwd = os.getcwd()
+    os.chdir(tdir)
+    dir1 = "Pancake/20130923/session01/array02/channel33"
+    dir2 = "Pancake/20130923/session01/array02/channel34"
+    for d in [dir1,dir2]:
+        if not os.path.isdir(d):
+            os.makedirs(d)
+    dirs = DPT.levels.get_level_dirs("channel", "Pancake/20130923/session01/array02")
+    assert dirs[0] == "Pancake/20130923/session01/array02/channel33"
+    assert dirs[1] == "Pancake/20130923/session01/array02/channel34"
+
+    dirs = DPT.levels.get_level_dirs("session", "Pancake/20130923/session01/array02")
+    assert dirs[0] == "Pancake/20130923/session01/array02/./../../session01"
+
+    dirs = DPT.levels.get_level_dirs("array", "Pancake/20130923/session01/array02")
+    assert dirs[0] == "Pancake/20130923/session01/array02/./../array02"
+
+    os.chdir(cwd)
