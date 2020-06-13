1f91c7405c962dde4f22934cffc5856ff32d14ae
roger <roger.herikstad@gmail.com>
2020-06-13 09:42:25 +0800

    Adds class for containing list of DPObjects

--- DataProcessingTools/objects.py
+++ DataProcessingTools/objects.py
@@ -19,3 +19,21 @@ class DPObject():
         Return `index` if it is valid for the current object
         """
         return max(0, min(index, self.data.shape[0]-1))
+
+
+class DPObjects():
+    def __init__(self, objects):
+        self.objects = objects
+
+    def __getitem__(self, key):
+        return self.objects[key]
+
+    def update_idx(self, index):
+        return max(0, min(index, len(self.objects)-1))
+
+    def append(self, item):
+        self.objects.append(item)
+
+    def plot(self, i, *args, **kwargs):
+        j = self.update_idx(i)
+        return self.objects[j].plot(*args, **kwargs)
