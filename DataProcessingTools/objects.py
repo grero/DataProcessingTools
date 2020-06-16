f946e8a145529c6faada4edfc60b41f130fc279b
roger <roger.herikstad@gmail.com>
2020-06-17 07:22:10 +0800

    Adds functionality for appending objects

--- DataProcessingTools/objects.py
+++ DataProcessingTools/objects.py
@@ -1,9 +1,12 @@
 import numpy as np
+from . import levels
 
 
 class DPObject():
     def __init__(self, *args, **kwargs):
         self.data = np.ndarray((0, 0))
+        self.dirs = []
+        self.setidx = []
 
     def plot(self, i, fig):
         pass
@@ -19,6 +22,32 @@ class DPObject():
         Return `index` if it is valid for the current object
         """
         return max(0, min(index, self.data.shape[0]-1))
+
+    def getindex(self, level):
+        """
+        Return an index into this object for the requested level.
+        """
+        level_names = [levels.get_level_name(level, d)
+                       for d in self.dirs]
+        unique_names = []
+        for l in level_names:
+            if l not in unique_names:
+                unique_names.append(l)
+
+        idx = np.zeros((len(self.setidx), ), dtype=np.int)
+        for i in range(len(self.setidx)):
+            idx[i] = unique_names.index(level_names[self.setidx[i]])
+        return idx
+
+    def append(self, obj):
+        """
+        Appends the data of `obj` to this object.
+        """
+        mx = self.setidx[-1]+1
+        for s in obj.setidx:
+            self.setidx.append(s+mx)
+        for d in obj.dirs:
+            self.dirs.append(d)
 
 
 class DPObjects():
