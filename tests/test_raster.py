aa0b4d77c2df3d4d8d6e376e3a48823a6fcad890
roger <roger.herikstad@gmail.com>
2020-06-25 07:28:17 +0800

    Adds test for appending rasters

--- tests/test_raster.py
+++ tests/test_raster.py
@@ -12,7 +12,8 @@ def test_basic():
                            1.1, 1.2, 1.3, 1.4, 2.5, 2.6])
     trial_events = np.array([0.1, 1.1])
 
-    raster = DPT.raster.Raster(-0.1, 0.5, alignto=trial_events, spiketimes=spiketimes)
+    raster = DPT.raster.Raster(-0.1, 0.5, alignto=trial_events, spiketimes=spiketimes,
+                               dirs=["session01/array01/channel001/cell01"])
     assert (raster.trialidx == [0, 0, 0, 0, 0, 1, 1, 1, 1]).all()
     assert np.isclose(raster.spiketimes, [0.0, 0.05, 0.2, 0.3, 0.4, 0.0, 0.1, 0.2, 0.3]).all()
 
@@ -23,3 +24,10 @@ def test_basic():
     x, y = ax.lines[0].get_data()
     assert np.isclose(raster.spiketimes, x).all()
     assert np.isclose(raster.trialidx, y).all()
+
+    raster.append(DPT.raster.Raster(-0.1, 0.5, alignto=trial_events, spiketimes=spiketimes,
+                                   dirs=["session01/array01/channel002/cell01"]))
+    cell_idx = raster.getindex("cell")
+    for i in range(2):
+        assert np.allclose(raster.spiketimes[cell_idx(i)], [0.0, 0.05, 0.2, 0.3, 0.4, 0.0, 0.1, 0.2, 0.3])
+        assert (raster.trialidx[cell_idx(i)] == [0, 0, 0, 0, 0, 1, 1, 1, 1]).all()
