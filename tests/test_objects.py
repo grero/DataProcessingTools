a156d2e7a727dbda4013253edb4b9b4a7939e00d
roger <roger.herikstad@gmail.com>
2020-07-02 16:12:15 +0800

    Updates tests

--- tests/test_objects.py
+++ tests/test_objects.py
@@ -1,47 +1,68 @@
 import DataProcessingTools as DPT
+import matplotlib.pylab as plt
+import numpy as np
+import tempfile
+import os
 
 
 class MyObj(DPT.objects.DPObject):
-    def __init__(self, dirs, *args, **kwargs):
+    argsList = ["bins"]
+
+    def __init__(self,  *args, **kwargs):
         DPT.objects.DPObject.__init__(self, *args, **kwargs)
-        self.dirs = dirs
-        for i in range(len(dirs)):
+        for i in range(len(self.dirs)):
             self.setidx.extend([i for j in range(3)])
+        self.plotopts = {"exponent": 1.0}
 
+    def plot(self, i=None,  ax=None):
+        if ax is None:
+            ax = plt.gca()
+        if not self.plotopts.get("overlay", False):
+            ax.clear()
+        x = np.array([0, 1, 2, 3])
+        y = x**self.plotopts["exponent"]
+        ax.plot(x, y)
 
-def test_level_idx():
-    obj = MyObj(dirs=["session01/array01/channel01/cell01",
-                      "session01/array01/channel01/cell02"])
+def test_plot():
+    obj = MyObj([0.1, 0.2, 0.3], normpath=False)
+    assert np.allclose(obj.args["bins"], [0.1, 0.2, 0.3])
+    ax = plt.gca()
+    ax.clear()
+    obj.plot(ax=ax)
+    x = np.array([0, 1, 2, 3])
+    xy = ax.lines[0].get_data()
+    assert np.allclose(xy[0], x)
+    assert np.allclose(xy[1], x**1.0)
+    obj.update_plotopts({"exponent": 2.0}, ax=ax)
+    xy = ax.lines[0].get_data()
+    assert np.allclose(xy[1], x**2.0)
+
+def test_append():
+    tempdir = tempfile.gettempdir()
+    dirs = ["Pancake/20130923/session01/array01/channel001/cell01",
+            "Pancake/20130923/session01/array01/channel001/cell02",
+            "Pancake/20130923/session01/array02/channel033/cell01",
+            "Pancake/20130923/session01/array02/channel034/cell01"]
+
+    with DPT.misc.CWD(tempdir):
+        for d in dirs:
+            if not os.path.isdir(d):
+                os.makedirs(d)
+
+        with DPT.misc.CWD(dirs[0]):
+            obj1 = MyObj([0.1, 0.2, 0.3])
+
+        for d in dirs[1:]:
+            with DPT.misc.CWD(d):
+                obj = MyObj([0.1, 0.2, 0.3])
+                obj1.append(obj)
+
+    assert obj1.setidx == [0, 0, 0, 1, 1, 1, 2, 2, 2, 3, 3, 3]
+    assert obj1.dirs == dirs
 
     # test trial
     idx = obj.getindex("trial")
     assert idx(0) == [0]
-
-    # test cell level
-    idx = obj.getindex("cell")
-    assert (idx(0) == [0, 1, 2]).all()
-    assert (idx(1) == [3, 4, 5]).all()
-
-    # test session level
-    idx = obj.getindex("session")
-    assert (idx(0) == [0, 1, 2, 3, 4, 5]).all()
-
-
-def test_append():
-
-    obj1 = MyObj(dirs=["session01/array01/channel001/cell01",
-                        "session01/array01/channel001/cell02"])
-
-    obj2 = MyObj(dirs=["session01/array02/channel033/cell01",
-                         "session01/array02/channel034/cell01"])
-
-    obj1.append(obj2)
-
-    assert obj1.setidx == [0, 0, 0, 1, 1, 1, 2, 2, 2, 3, 3, 3]
-    assert obj1.dirs == ["session01/array01/channel001/cell01",
-                         "session01/array01/channel001/cell02",
-                         "session01/array02/channel033/cell01",
-                         "session01/array02/channel034/cell01"]
 
     idx = obj1.getindex("cell")
     assert (idx(0) == [0, 1, 2]).all()
@@ -53,6 +74,10 @@ def test_append():
     assert (idx(1) == [6, 7, 8]).all()
     assert (idx(2) == [9, 10, 11]).all()
 
+    idx = obj1.getindex("array")
+    assert (idx(0) == [0, 1, 2, 3, 4, 5]).all()
+    assert (idx(1) == [6, 7, 8, 9, 10, 11]).all()
+
     idx = obj1.getindex(None)
     assert idx(0) is None
 
@@ -63,8 +88,8 @@ def test_object():
         argsList = ["tmin", "tmax"]
         filename = "test.mat"
 
-    obj = MyObj2(-0.1, 1.0)
+    obj = MyObj2(-0.1, 1.0, normpath=False)
 
     assert obj.args["tmin"] == -0.1
     assert obj.args["tmax"] == 1.0
-    
+
