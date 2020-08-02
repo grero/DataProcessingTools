39370117dadc6d9d6e7136a7bef9d211be8404b6
roger <roger.herikstad@gmail.com>
2020-08-02 09:41:21 +0800

    Adds tests for processDirs

--- tests/test_misc.py
+++ tests/test_misc.py
@@ -10,3 +10,28 @@ def test_basic():
     with DPT.misc.CWD(tempdir):
         assert os.path.samefile(os.getcwd(), tempdir)
     assert os.path.samefile(os.getcwd(), cwd)
+
+
+def test_procesDirs():    
+    dirs = ["dir_{0}".format(i) for i in range(5)]
+
+    @DPT.misc.processDirs(dirs)
+    def testfunc():
+        with open("test1.txt", "w") as ff:
+            ff.write("sometest\n")
+
+    tempdir = tempfile.gettempdir()
+    with DPT.misc.CWD(tempdir):
+        for d in dirs:
+            if not os.path.isdir(d):
+                os.mkdir(d)
+        testfunc()
+        allok = True
+        for d in dirs:
+            with DPT.misc.CWD(d):
+                allok = allok and os.path.isfile("test1.txt")
+                if allok:
+                    os.remove("test1.txt")
+            os.rmdir(d)
+        assert allok
+
